467bb429ab89a1683214ac0e92a04711b293ebe8
piyush31508 <chawlapiyush811@gmail.com>
2025-12-07 16:30:59 +0530

    Added Protected Routes for admin access

--- model/user.js
+++ model/user.js
@@ -1,15 +1,22 @@
+// model/user.js
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/
-    }
-},
-{
-    timestamps:true
-})
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,           
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const User = mongoose.model("User", userSchema);
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,15 +7,19 @@ import {
   deleteProduct,
   searchProducts,
 } from '../controller/productController.js';
+import { isAuth } from '../middleware/isAuth.js';
+import { isAdmin } from '../middleware/isAdmin.js';
 
 const router = express.Router();
 
-router
-  .post('/create', createProduct)
-  .get('/all', getAllProducts)
-  .get('/search', searchProducts) 
-  .get('/:id', getProductById)
-  .put('/:id', updateProduct)
-  .delete('/:id', deleteProduct);
+// public
+router.get('/all', getAllProducts);
+router.get('/search', searchProducts);
+router.get('/:id', getProductById);
+
+// admin-only
+router.post('/create', isAuth, isAdmin, createProduct);
+router.put('/:id', isAuth, isAdmin, updateProduct);
+router.delete('/:id', isAuth, isAdmin, deleteProduct);
 
 export default router;
